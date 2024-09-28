580769b77104cde5df9c9b3703d3b14365168b20
Pranay vyas <pranayvyas123@gmail.com>
2024-09-28 15:50:35 +0530

    update

--- server.js
+++ server.js
@@ -2,23 +2,13 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/*": "/$1",
- })
-);
+
 
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow any origin
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header("Access-Control-Allow-Origin", "*");
     next();
   });
 
