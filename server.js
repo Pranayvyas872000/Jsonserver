32f1178e1b8967dc2c28887980c4204d0a9be9b3
Pranay vyas <pranayvyas123@gmail.com>
2024-09-29 19:59:49 +0530

    update

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.default();
+const router = jsonServer.router('db.json');
 
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("tmp/db.json");
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
+server.use(jsonServer.bodyParser);
 server.use(router);
-// Listen to port
+
 server.listen(3000, () => {
- console.log("JSON Server is running");
+  console.log('JSON Server is running on port 3000');
 });
-
-// Export the Server API
-module.exports = server;
-
-
-
